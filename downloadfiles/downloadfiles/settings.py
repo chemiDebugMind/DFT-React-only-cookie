b2bbba63973f4154ff520b5312c8f862ec0ade31
chemiDebugMind <tenzinchemi50@gmail.com>
2023-03-27 10:51:17 +0530

    Add Procfile,requirement.txt and runtime.txt files for deployment
    Created three files for the deployment of django project

--- downloadfiles/downloadfiles/settings.py
+++ downloadfiles/downloadfiles/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#6!n!!)1i3&457!kn^ly8olgt_7$n-8fsn4eo#e*hn+qg)m^%b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
