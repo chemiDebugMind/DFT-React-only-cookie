1ae0d09031734d51b370678df5d39fa21bb707c6
chemiDebugMind <tenzinchemi50@gmail.com>
2023-03-27 12:37:30 +0530

    staticfiles set

--- downloadfiles/downloadfiles/settings.py
+++ downloadfiles/downloadfiles/settings.py
@@ -122,11 +122,9 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'downloadfiles/static/')
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
