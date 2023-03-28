f3980b6aa8d6d2a16299a12a79a09d540f0a0c2e
chemiDebugMind <tenzinchemi50@gmail.com>
2023-03-28 09:32:38 +0530

    cors allowed origin set

--- downloadfiles/downloadfiles/settings.py
+++ downloadfiles/downloadfiles/settings.py
@@ -137,14 +137,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5173",
+    "https://dft-react-only-cookie.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTP_ONLY = True
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:5173",
+    "https://dft-react-only-cookie.vercel.app",
 
 ]
 CORS_EXPOSE_HEADERS = ["Content-Type", "X-CSRFToken"]
